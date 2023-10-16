04712084e00fdbe0b9f6202846cbfac078a07e35
JCGutierrezConcha <jc.gutierrez.concha@gmail.com>
2023-10-16 15:17:33 -0300

    Homework-5

---
+++ code-hw-5/q6_predict.py
@@ -0,0 +1,31 @@
+import pickle
+
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+def load(filename: str):
+    with open(filename, 'rb') as f_in:
+        return pickle.load(f_in)
+
+
+dv = load('dv.bin')
+model = load('model2.bin')
+
+app = Flask('bank-score')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    client = request.get_json()
+
+    X = dv.transform([client])
+    y_pred = model.predict_proba(X)[0, 1]
+    get_score = y_pred >= 0.5
+
+    result = {
+        'get_score_probability': float(y_pred),
+        'get_score': bool(get_score)
+    }
+
+    return jsonify(result)
